49513dc5e56b67f809f5c5906af5548f47aa2872
Marcus Hansson <marcus.hansson@email.com>
2025-05-17 18:18:49 +0200

    Update markup to close resemble the actual opsgenie looks

--- firefox/js/sidebar.js
+++ firefox/js/sidebar.js
@@ -1,4 +1,4 @@
-import { OPSGENIE_DOMAIN, defaultSettings } from "./lib/shared.js";
+import { OPSGENIE_DOMAIN, defaultSettings, alert } from "./lib/shared.js";
 import {h, app, text} from "./lib/hyperapp.js";
 import {
     body, main, article, section, aside,
@@ -15,56 +15,73 @@ const Priority = {
 	P5: 0
 };
 
+const Highlights = {
+	2: "red",
+	1: "yellow",
+	0: "green"
+};
+
 const g = (m) => browser.i18n.getMessage(m);
 const gt = (m) => text(g(m));
 
 const api = () => browser.storage.session.get("api");
 
-const description = (message) => (
-	message = message.split(":"),
-	message[message.length - 1].trim()
-);
+const description = (message) => message;
 
 const onUpdate = ({...state}) => [
 	{...state},
+	[Promisable("settings"), browser.storage.sync.get(defaultSettings)],
 	[Promisable("api"), api()]
 ];
+
+const onOptions = ({...state}) => (
+	browser.runtime.sendMessage({ action: "options" }),
+	{...state}
+);
+
+const start = (state) => (
+	browser.runtime.sendMessage({
+		action: "start"
+	}),
+	{...state}
+);
 
 const onConfigChg = (key) => ({settings, ...state}, value) => (
 	settings = {...settings, [key]: value},
 	browser.storage.sync.set(settings),
 	{
 		...state,
-		settings: {
-			...settings,
-			[key]: value
-		}
+		settings
 	}
 );
 
 const event = fn => (state, e) => [fn, e.target.value];
 
-const query = ({api}) =>
+const query = ({api, settings}) =>
 	api.query.data.length &&
 	ul([
 		...api.query.data
 			.sort((a, b) => Priority[a.priority] < Priority[b.priority])
 			.map(
-				alert =>
+				item =>
 					li([
 						a({
-							href:"https://duckduckgo.com/",
-							"data-tooltip": description(alert.message),
+							href: alert(settings, item.id),
+							target: "_black",
+							"data-tooltip": description(item.message),
 							"data-flow": "bottom"
 						}, [
-							span({class:{badge: true, yes: alert.acknowledged}}, [
+							span({class:{badge: true, mono: true, [Highlights[Priority[item.priority]]]: true}}, [
+								text(item.priority)
+							]),
+							span({class:{badge: true, yes: item.acknowledged}}, [
 								text("ACK")
 							]),
 							span({class:{badge: true, yes: true}}, [
-								text(alert.count + "x")
+								text(item.count + "x")
 							]),
 							span({class:"message"}, [
-								text(alert.message)
+								text(item.message)
 							])
 						])
 					])
@@ -80,36 +97,24 @@ const error = ({api}) =>
 	]);
 
 const render = ({api, settings}) =>
-	settings &&
+	!settings && main([gt("loading")]) ||
 	main([
-		details([
-			fieldset([
-				summary([
-					gt("settings")
+		fieldset([
+			summary([
+				gt("Control")
+			]),
+			
+			section([
+				input({type:"button", value:g("options"), onclick:onOptions}, [
+					gt("options")
 				]),
-
+				
 				input({type:"button", value:g("refresh"), onclick:onUpdate}, [
 					gt("refresh")
 				]),
-
-				label([
-					gt("interval"),
-					input({
-						type: "number",
-						min: 1000,
-						max: 60000,
-						value: settings.timeInterval,
-						oninput:event(onConfigChg("timeInterval"))
-					})
-				]),
-
-				label([
-					gt("enable"),
-					input({
-						type: "checkbox",
-						checked: settings.enabled,
-						oninput:event(onConfigChg("enabled"))
-					})
+				
+				input({type:"button", value:g("start"), onclick:start}, [
+					gt("start")
 				])
 			])
 		]),
@@ -132,7 +137,7 @@ const render = ({api, settings}) =>
 
 const dispatch = app({
 	init: [
-		{settings: {...defaultSettings}},
+		{},
 		[Promisable("settings"), browser.storage.sync.get(defaultSettings)]
 	],
 
@@ -140,12 +145,14 @@ const dispatch = app({
 	node: document.getElementsByTagName("main")[0]
 });
 
+browser.storage.sync.onChanged.addListener(
+	async () => {
+		dispatch(onUpdate)
+	}
+);
+
 browser.storage.session.onChanged.addListener(
 	async () => {
 		dispatch(onUpdate);
 	}
 );
-
-browser.runtime.sendMessage({
-	action: "start"
-});
